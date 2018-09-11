c281c79516689bdde9e8cb2f3c2af286ff691444
Eugene-Sviridov <smart8410@gmail.com>
2018-09-11 03:42:52 +0300

    init commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'jekyll'
+
+group :jekyll_plugins do
+  gem 'jekyll-autoprefixer'
+  gem 'jekyll-seo-tag'
+end
---
+++ _site/js/main.js
@@ -0,0 +1,40 @@
+const getDays = (date) => {
+  return Math.floor(date/(1000*60*60*24));
+}
+
+const getDiffArray = () => {
+  var targetDate = new Date(Date.UTC(2018, 9, 20, 12, 30)), // 20 October 12:30
+      currentDate = new Date(),
+      diff = new Date(targetDate - currentDate);
+
+  return [
+      Math.floor(getDays(diff)) * 24 + new Date(diff).getHours(),
+      new Date(diff).getMinutes(),
+      new Date(diff).getSeconds()
+  ];
+}
+
+const decreaseCountdown = ($items) => {
+  const diff = getDiffArray();
+
+  if (!diff) { return false; }
+
+  diff.forEach((item, index) => {
+    $items.eq(index).text(item);
+  })
+  
+  setTimeout(() => {
+      decreaseCountdown($items);
+  }, 1000);
+}
+
+$(document).ready(function() {
+  var $headerTimers = $('.s-hero__form-timer-time-item'),
+      $footerTimers = $('.s-form__form-timer-time-item');
+
+  decreaseCountdown($headerTimers);
+  decreaseCountdown($footerTimers);
+
+
+
+})

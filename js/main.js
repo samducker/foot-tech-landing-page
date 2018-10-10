0ec5661f8ebcd028f0981b0a8b581b79b664fb49
Andrey Nikonov <a.einomi@gmail.com>
2018-10-11 01:56:20 +0300

    Fix countdown

--- js/main.js
+++ js/main.js
@@ -1,29 +1,52 @@
-const getDays = (date) => {
-  return Math.floor(date/(1000*60*60*24));
+function msToDays(value) {
+  return Math.floor(value / (1000 * 3600 * 24));
 }
 
-const getDiffArray = () => {
-  var targetDate = new Date(Date.UTC(2018, 8, 29, 00, 00)), // 29 September 12 AM
-      currentDate = new Date(),
-      diff = new Date(targetDate - currentDate);
+function daysToMs(value) {
+  return value * 1000 * 3600 * 24;
+}
+
+function msToMinutes(value) {
+  return Math.floor(value / (1000 * 60));
+}
+
+function minutesToMs(value) {
+  return value * 1000 * 60;
+}
+
+function msToSeconds(value) {
+  return Math.floor(value / 1000);
+}
+
+function getDiffArray () {
+  var targetDate = new Date(2018, 9, 30, 0, 0); // 30 October 12 AM
+  var currentDate = new Date();
+
+  var diff = targetDate - currentDate;
+
+  var days = msToDays(diff);
+
+  var daysMs = daysToMs(days);
+  var minutes = msToMinutes(diff - daysMs);
+
+  var minutesMs = minutesToMs(minutes);
+  var seconds = msToSeconds(diff - daysMs - minutesMs);
 
   return [
-      Math.floor(getDays(diff)) * 24 + new Date(diff).getHours(),
-      new Date(diff).getMinutes(),
-      new Date(diff).getSeconds()
+    days,
+    minutes,
+    seconds,
   ];
 }
 
-const decreaseCountdown = ($items) => {
-  const diff = getDiffArray();
-
-  if (!diff) { return false; }
+function decreaseCountdown($items) {
+  var diff = getDiffArray();
 
   diff.forEach((item, index) => {
     $items.eq(index).text(item);
-  })
+  });
 
-  setTimeout(() => {
+  setTimeout(function() {
       decreaseCountdown($items);
   }, 1000);
 }
@@ -34,7 +57,4 @@ $(document).ready(function() {
 
   decreaseCountdown($headerTimers);
   decreaseCountdown($footerTimers);
-
-
-
-})
+});
